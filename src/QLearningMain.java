2cf39c3e6659520146568c37bcbd0d9a87e3a1b4
theaniartist <wsly@wpi.edu>
2022-02-21 20:23:59 -0500

    Add file parser

--- src/QLearningMain.java
+++ src/QLearningMain.java
@@ -1,5 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class QLearningMain {
+
     public static void main(String[] args) {
 
+        String filename = args[0];
+        float timeToRun = Float.parseFloat(args[1]);
+        float desiredProb = Float.parseFloat(args[2]);
+        float reward = Float.parseFloat(args[3]);
+
+        int rows = 0;
+        int cols = 0;
+        List<String []> elements = new ArrayList<>();
+
+        try {
+            File myObj = new File(filename);
+            Scanner scanner = new Scanner(myObj);
+            while (scanner.hasNextLine()) {
+                rows += 1;
+                String data = scanner.nextLine();
+                String[] values = data.split("\\t");
+                elements.add(values);
+                cols = values.length;
+            }
+            int [][] grid = createGrid(elements, rows, cols);
+            scanner.close();
+        } catch (IOException i) {
+            System.out.println("File Exception");
+        }
+    }
+
+    public static int[][] createGrid(List<String []> values, int row, int col) {
+        int [][] grid = new int [row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                grid [i][j] = Integer.parseInt(values.get(i)[j]);
+            }
+        }
+        return grid;
     }
 }
